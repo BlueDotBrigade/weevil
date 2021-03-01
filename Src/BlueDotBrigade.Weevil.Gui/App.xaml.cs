5c3fe0c54ce724f80e35741db3e824d0d93a3d38
Pressacco <5507864+pressacco@users.noreply.github.com>
2021-03-01 18:34:18 -0500

    Uncaught exceptions are now being logged before the applicaiton crashes. (#34)

--- Src/BlueDotBrigade.Weevil.Gui/App.xaml.cs
+++ Src/BlueDotBrigade.Weevil.Gui/App.xaml.cs
@@ -1,7 +1,9 @@
 ﻿namespace BlueDotBrigade.Weevil.Gui
 {
+	using System;
 	using System.Runtime.ExceptionServices;
 	using System.Security;
+	using System.Threading.Tasks;
 	using System.Windows;
 	using System.Windows.Threading;
 	using BlueDotBrigade.Weevil.Diagnostics;
@@ -18,17 +20,17 @@
 		[HandleProcessCorruptedStateExceptions]
 		private void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
 		{
-			var errorMsg = string.Format("An exception thrown by the WPF UI thread does not have an appropriate handler. IsHandled={0}", e.Handled);
+			var errorMessage = string.Format("An exception thrown by the WPF UI thread does not have an appropriate handler. IsHandled={0}", e.Handled);
 
 			try
 			{
 				if (e.Exception == null)
 				{
-					Log.Default.Write(LogSeverityType.Critical, errorMsg);
+					Log.Default.Write(LogSeverityType.Critical, errorMessage);
 				}
 				else
 				{
-					Log.Default.Write(LogSeverityType.Critical, e.Exception, errorMsg);
+					Log.Default.Write(LogSeverityType.Critical, e.Exception, errorMessage);
 				}
 			}
 			catch
@@ -38,11 +40,65 @@
 			}
 		}
 
+		[SecurityCritical]
+		[HandleProcessCorruptedStateExceptions]
+		private void OnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+		{
+			var exception = e.ExceptionObject as Exception;
+
+			var errorMessage = string.Format("An unhandled .Net exception is forcing the application to terminate unexpectedly.");
+
+			try
+			{
+				if (exception == null)
+				{
+					Log.Default.Write(LogSeverityType.Critical, errorMessage);
+				}
+				else
+				{
+					Log.Default.Write(LogSeverityType.Critical, exception, errorMessage);
+				}
+			}
+			catch
+			{
+				// Nothing to do
+				// ... memory allocations and method calls should be kept to a minimum
+			}
+		}
+
+		[SecurityCritical]
+		[HandleProcessCorruptedStateExceptions]
+		private void OnUnhandledTplException(object sender, UnobservedTaskExceptionEventArgs e)
+		{
+			var errorMessage = string.Format("An unhandled TPL exception is forcing the application to terminate unexpectedly.");
+
+			try
+			{
+				if (e.Exception == null)
+				{
+					Log.Default.Write(LogSeverityType.Critical, errorMessage);
+				}
+				else
+				{
+					Log.Default.Write(LogSeverityType.Critical, e.Exception, errorMessage);
+				}
+			}
+			catch
+			{
+				// Nothing to do
+				// ... memory allocations and method calls should be kept to a minimum
+			}
+		}
+
+		[SecurityCritical]
+		[HandleProcessCorruptedStateExceptions]
 		private void OnApplicationOpening(object sender, StartupEventArgs e)
 		{
 			try
 			{
 				Application.Current.DispatcherUnhandledException += OnDispatcherUnhandledException;
+				AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
+				TaskScheduler.UnobservedTaskException += OnUnhandledTplException;
 
 				Log.Default.Write(LogSeverityType.Information, "Weevil application is starting...");
 				Log.Register(new NLogWriter());
