053d82b63f5e599047a8a19dcf44c7729d13f90e
Pressacco <5507864+pressacco@users.noreply.github.com>
2021-03-15 19:08:32 -0400

    Test: ensuring that records are being truncated properly when the content is too long. (#87)

---
+++ Tst/BlueDotBrigade.Weevil.Gui-UiTests/Converters/ContentConverterTests.cs
@@ -0,0 +1,50 @@
+﻿namespace BlueDotBrigade.Weevil.Gui.Converters
+{
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	[TestClass]
+	public class ContentConverterTests
+	{
+		[TestMethod]
+		public void TruncateIf_ContentIsNotTooLong_ReturnsContent()
+		{
+			var result = ContentConverter.TruncateIf(
+				content: "The quick brown fox jumps over the lazy dog.",
+				isSingleLine: true,
+				maximumLength: 45,
+				truncatedLength: 19);
+
+			Assert.AreEqual(
+				"The quick brown fox jumps over the lazy dog.",
+				result);
+		}
+
+		[TestMethod]
+		public void TruncateIf_ContentIsTooLong_ReturnsShortenedString()
+		{
+			var result = ContentConverter.TruncateIf(
+				content: "The quick brown fox jumps over the lazy dog.",
+				isSingleLine:true,
+				maximumLength: 44,
+				truncatedLength:19);
+
+			Assert.AreEqual(
+				"The quick brown fox" + ContentConverter.EndOfLine,
+				result);
+		}
+
+		[TestMethod]
+		public void TruncateIf_MultiLineContentIsTooLong_ReturnsContent()
+		{
+			var result = ContentConverter.TruncateIf(
+				content: "The quick brown fox jumps over the lazy dog.",
+				isSingleLine: false,
+				maximumLength: 44,
+				truncatedLength: 19);
+
+			Assert.AreEqual(
+				"The quick brown fox jumps over the lazy dog.",
+				result);
+		}
+	}
+}
