d5d1c5029e0cec723298c2ea5749b2584b3054ce
Pressacco <5507864+pressacco@users.noreply.github.com>
2021-03-23 20:11:46 -0400

    Created a new `InsightControl` which is used by the `DashboardDialog` (#20)

---
+++ Src/BlueDotBrigade.Weevil.Gui/Analysis/DashboardDialog.xaml.cs
@@ -0,0 +1,49 @@
+﻿namespace BlueDotBrigade.Weevil.Gui.Analysis
+{
+	using System;
+	using System.Collections.Immutable;
+	using System.Windows;
+	using BlueDotBrigade.Weevil.Analysis;
+
+	/// <summary>
+	/// Interaction logic for DashboardDialog.xaml
+	/// </summary>
+	public partial class DashboardDialog : Window
+	{
+		public static readonly DependencyProperty InsightsProperty =
+			DependencyProperty.Register(
+				nameof(Insights), typeof(IInsight[]),
+				typeof(DashboardDialog));
+
+		public static readonly DependencyProperty EngineProperty =
+			DependencyProperty.Register(
+				nameof(Engine), typeof(IEngine),
+				typeof(DashboardDialog));
+
+		public IInsight[] Insights
+		{
+			get => (IInsight[])GetValue(InsightsProperty);
+			set => SetValue(InsightsProperty, value);
+		}
+
+		public IEngine Engine
+		{
+			get => (IEngine)GetValue(EngineProperty);
+			set => SetValue(EngineProperty, value);
+		}
+
+		public DashboardDialog()
+		{
+			InitializeComponent();
+			this.DataContext = this;
+		}
+
+		private void OnRefresh(object sender, RoutedEventArgs e)
+		{
+			foreach (IInsight insight in this.Insights)
+			{
+				insight.Refresh(Engine.Records);
+			}
+		}
+	}
+}
---
+++ Src/BlueDotBrigade.Weevil.Gui/Analysis/InsightControl.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace BlueDotBrigade.Weevil.Gui.Analysis
+{
+	using System.Windows;
+	using System.Windows.Controls;
+	using BlueDotBrigade.Weevil.Analysis;
+
+	/// <summary>
+	/// Interaction logic for InsightControl.xaml
+	/// </summary>
+	public partial class InsightControl : UserControl
+	{
+		public static readonly DependencyProperty ValueProperty =
+			DependencyProperty.Register(
+				nameof(Value),
+				typeof(IInsight),
+				typeof(InsightControl));
+
+		public IInsight Value
+		{
+			get => (IInsight)GetValue(ValueProperty);
+			set => SetValue(ValueProperty, value);
+		}
+
+		public InsightControl()
+		{
+			InitializeComponent();
+		}
+	}
+}
