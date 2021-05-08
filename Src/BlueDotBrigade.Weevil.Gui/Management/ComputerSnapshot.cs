64cbba521461f3b791e56aee3bede543642ad904
Pressacco <5507864+pressacco@users.noreply.github.com>
2021-05-08 18:39:38 -0400

    New: added more properties to `ComputerSnapshot` (#125)

--- Src/BlueDotBrigade.Weevil.Gui/Management/ComputerSnapshot.cs
+++ Src/BlueDotBrigade.Weevil.Gui/Management/ComputerSnapshot.cs
@@ -8,45 +8,51 @@
 	class ComputerSnapshot
 	{
 		private const string DefaultString = "Unknown";
-		//private const uint DefaultInt = -1;
 
 		public ComputerSnapshot()
 		{
 			this.CpuName = DefaultString;
 			this.CpuProcessors = 0;
 			this.CpuCores = 0;
-			this.OperatingSystem = DefaultString;
+			this.OsName = DefaultString;
 			this.CpuName = DefaultString;
 			this.CpuName = DefaultString;
-			this.RamTotal = new StorageUnit();
+			this.RamTotalInstalled = new StorageUnit();
 		}
 
 		public string CpuName { get; private set; }
 
-
 		public uint CpuProcessors { get; private set; }
 		public uint CpuCores { get; private set; }
 
-		public string OperatingSystem { get; private set; }
+		public string OsName { get; private set; }
+		public string OsVersion { get; private set; }
+		public bool OsIs64Bit { get; private set; }
+		public DateTime OsInstalledAt { get; private set; }
+		public DateTime OsBootedAt { get; private set; }
 
-		public StorageUnit RamTotal { get; private set; }
+		/// <summary>
+		/// Total amount of physical memory available to the operating system.
+		/// This value does not necessarily indicate the true amount of physical memory,
+		/// but what is reported to the operating system as available to it.
+		/// </summary>
+		public StorageUnit RamTotalInstalled { get; private set; }
+
+		/// <summary>
+		/// Represents the amount of physical memory that is not currently in use.
+		/// </summary>
+		public StorageUnit RamTotalFree { get; private set; }
 
 		public uint RamSlotsUsed { get; private set;}
 
-		public override string ToString()
-		{
-			return
-				$"{nameof(OperatingSystem)}=`{this.OperatingSystem}`, " +
-				$"{nameof(CpuName)}=`{this.CpuName}`, " +
-				$"{nameof(CpuProcessors)}={this.CpuProcessors}, " +
-				$"{nameof(CpuCores)}={this.CpuCores}, " +
-				$"{nameof(this.RamTotal)}={this.RamTotal.GigaBytes:0.00}GB";
-		}
-
 		public static ComputerSnapshot Create()
 		{
-			var snapshot = new ComputerSnapshot();
-
+			var snapshot = new ComputerSnapshot
+			{
+				OsName = Environment.OSVersion.ToString(),
+				OsIs64Bit = Environment.Is64BitOperatingSystem,
+			};
+			
 			try
 			{
 				var cpuQuery = new SelectQuery("SELECT * FROM Win32_Processor");
@@ -66,23 +72,54 @@
 						}
 					}
 				}
-				var ramQuery = new SelectQuery("select * from Win32_PhysicalMemory");
 
-				using (var searcher = new ManagementObjectSearcher(ramQuery))
+				var osQuery = new SelectQuery("select * from Win32_OperatingSystem");
+
+				using (var searcher = new ManagementObjectSearcher(osQuery))
 				{
-					using (ManagementObjectCollection memorySticks = searcher.Get())
+					using (ManagementObjectCollection results = searcher.Get())
 					{
-						snapshot.RamSlotsUsed = (uint)memorySticks.Count;
-
-						foreach (ManagementBaseObject memoryStick in memorySticks)
+						foreach (ManagementBaseObject result in results)
 						{
-							var managementObject = (ManagementObject)memoryStick;
+							var managementObject = (ManagementObject)result;
 
-							var capacity = (ulong)managementObject["Capacity"];
-							snapshot.RamTotal += new StorageUnit(capacity);
+							snapshot.OsName = managementObject["Caption"] as string;
+							snapshot.OsVersion = managementObject["Version"] as string;
+
+							var installDate = managementObject["InstallDate"].ToString();
+							snapshot.OsInstalledAt = ManagementDateTimeConverter.ToDateTime(installDate);
+
+							var bootedAt = managementObject["LastBootUpTime"].ToString();
+							snapshot.OsBootedAt = ManagementDateTimeConverter.ToDateTime(bootedAt);
+
+							var totalVisibleKb = (ulong)managementObject["TotalVisibleMemorySize"];
+							snapshot.RamTotalInstalled += new StorageUnit(totalVisibleKb * StorageUnit.BytesPerKilobyte);
+
+							var totalFreeKb = (ulong)managementObject["FreePhysicalMemory"];
+							snapshot.RamTotalFree += new StorageUnit(totalFreeKb * StorageUnit.BytesPerKilobyte);
+
+							break;
 						}
 					}
 				}
+
+				//var ramQuery = new SelectQuery("select * from Win32_PhysicalMemory");
+
+				//using (var searcher = new ManagementObjectSearcher(ramQuery))
+				//{
+				//	using (ManagementObjectCollection memorySticks = searcher.Get())
+				//	{
+				//		snapshot.RamSlotsUsed = (uint)memorySticks.Count;
+
+				//		foreach (ManagementBaseObject memoryStick in memorySticks)
+				//		{
+				//			var managementObject = (ManagementObject)memoryStick;
+
+				//			var capacity = (ulong)managementObject["Capacity"];
+				//			snapshot.RamTotalInstalled += new StorageUnit(capacity);
+				//		}
+				//	}
+				//}
 			}
 			catch (Exception e)
 			{
@@ -91,7 +128,6 @@
 					e,
 					"An unexpected error occurred while taking a snapshot of the computer hardware.");
 			}
-			snapshot.OperatingSystem = Environment.OSVersion.ToString();
 
 			return snapshot;
 		}
