d8caca8d11cecec92f35ebff4572ecb83d0b2ae5
Pressacco <5507864+pressacco@users.noreply.github.com>
2021-10-02 17:25:43 -0400

    New : the `R` record factory can now create a collection of records #150

--- Tst/BlueDotBrigade.Weevil.Core-UnitTests/Data/R.cs
+++ Tst/BlueDotBrigade.Weevil.Core-UnitTests/Data/R.cs
@@ -1,12 +1,62 @@
 ﻿namespace BlueDotBrigade.Weevil.Data
 {
 	using System;
+	using System.Collections.Generic;
+	using System.Collections.Immutable;
 
 	/// <summary>
-	/// A record factory used to create fake data for testing.
+	/// A record factory that uses convention over configuration
+	/// to create fake records for testing.
 	/// </summary>
-	internal static class R
+	internal class R
 	{
+		private int _lineNumber = 0;
+
+		private readonly IList<IRecord> _records;
+
+		private R()
+		{
+			_records = new List<IRecord>();
+		}
+
+		public static R Create()
+		{
+			return new R();
+		}
+
+		public ImmutableArray<IRecord> GetRecords()
+		{
+			return _records.ToImmutableArray();
+		}
+
+		/// <summary>
+		/// Creates a fake record with the given <paramref name="timestamp"/>.
+		/// </summary>
+		/// <remarks>
+		/// Default values:
+		/// <list type="bullet">
+		/// <item> Line number starts at 1, and increments by 1.</item>
+		/// </list>
+		/// </remarks>
+		public R WithCreatedAt(string timestamp)
+		{
+			_lineNumber++;
+			return WithCreatedAt(_lineNumber, timestamp);
+		}
+		public R WithCreatedAt(int lineNumber, string timestamp)
+		{
+			var record = new Record(
+				lineNumber,
+				DateTime.Parse(timestamp),
+				SeverityType.Information,
+				$"Fake record used for testing. Has line number: {_lineNumber}",
+				new Metadata());
+
+			_records.Add(record);
+
+			return this;
+		}
+
 		/// <summary>
 		/// Creates a fake record with the provided <paramref name="lineNumber"/>.
 		/// </summary>
