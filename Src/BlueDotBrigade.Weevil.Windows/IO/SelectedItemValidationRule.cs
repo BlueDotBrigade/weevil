4e87e7472003f64de3c2140c360660886520d241
Pressacco <5507864+pressacco@users.noreply.github.com>
2021-11-20 09:12:34 -0500

    New : Added a new validator to ensure that a ComboBox item has been selected. `SelectedItemValidationRule` #204

---
+++ Src/BlueDotBrigade.Weevil.Windows/IO/SelectedItemValidationRule.cs
@@ -0,0 +1,61 @@
+﻿namespace BlueDotBrigade.Weevil.Windows.IO
+{
+	using System.Globalization;
+	using System.Windows.Controls;
+
+	/// <summary>
+	/// Validation fails when a ComboBox item has not been selected.
+	/// </summary>
+	/// <remarks>
+	///  The following sample code shows how to use the validator in XAML:
+	/// <code>
+	/// <![CDATA[
+	/// <ComboBox ItemsSource="{Binding ListOfOptions}">
+	///		<ComboBox.SelectedValue>
+	///			<Binding Path = "SelectedOption" UpdateSourceTrigger="PropertyChanged">
+	///				<Binding.ValidationRules>
+	///					<SelectedItemValidationRule
+	///						ValidatesOnTargetUpdated = "True"
+	///						ErrorMessage="Custom error message goes here."/>
+	///				</Binding.ValidationRules>
+	///			</Binding>
+	///		</ComboBox.SelectedValue>
+	///	</ComboBox>
+	/// ]]>
+	/// </code>
+	/// </remarks>
+	public class SelectedItemValidationRule : ValidationRule
+	{
+		private static readonly object NoItemSelected = null;
+
+		private const string DefaultMessage = @"A ComboBox item should be selected.";
+
+		/// <summary>
+		/// Validation fails when a ComboBox item has not been selected.
+		/// </summary>
+		public SelectedItemValidationRule()
+		{
+			this.ErrorMessage = DefaultMessage;
+
+			Validate(NoItemSelected, CultureInfo.InvariantCulture);
+		}
+
+		public string ErrorMessage { get; set; }
+
+		/// <summary>
+		/// Returns a validation error when a ComboBox item has not been selected.
+		/// </summary>
+		/// <param name="selectedValue">Bound to the SelectedValue property of a WPF ComboBox.</param>
+		/// <param name="cultureInfo">The culture to use with this rule.</param>
+		/// <returns></returns>
+		/// <seealso cref="ComboBox"/>
+		public override ValidationResult Validate(object selectedValue, CultureInfo cultureInfo)
+		{
+			var isItemSelected = selectedValue != NoItemSelected;
+
+			return isItemSelected
+				? new ValidationResult(true, null)
+				: new ValidationResult(false, this.ErrorMessage);
+		}
+	}
+}
