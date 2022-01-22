c34033ecc78969162ba76b50c61e777dbcd703c4
Pressacco <5507864+pressacco@users.noreply.github.com>
2022-01-22 08:17:07 -0500

    Test : Added unit tests to verify that `Find` content works for case senstive / insensitive searches. #235

---
+++ Tst/BlueDotBrigade.Weevil.Core-UnitTests/Navigation/ContentNavigatorTests.cs
@@ -0,0 +1,41 @@
+﻿namespace BlueDotBrigade.Weevil.Navigation
+{
+	using BlueDotBrigade.Weevil.Data;
+	using BlueDotBrigade.Weevil.TestingTools.Data;
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	[TestClass]
+	public class ContentNavigatorTests
+	{
+		[TestMethod]
+		public void FindNext_CaseIgnored_RecordFound()
+		{
+			var records = R.Create()
+				.WithContent("The quick brown fox")
+				.WithContent("jumps OVER")
+				.WithContent("the lazy dog")
+				.GetRecords();
+
+			var activeRecord = new ActiveRecord(records);
+			var result = new ContentNavigator(activeRecord).FindNext("over", isCaseSensitive: false);
+
+			Assert.AreEqual(2, result.LineNumber);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(RecordNotFoundException))]
+		public void FindNext_CaseSensitive_Throws()
+		{
+			var records = R.Create()
+				.WithContent("The quick brown fox")
+				.WithContent("jumps OVER")
+				.WithContent("the lazy dog")
+				.GetRecords();
+
+			var activeRecord = new ActiveRecord(records);
+			var result = new ContentNavigator(activeRecord).FindNext("over", isCaseSensitive: true);
+
+			Assert.Fail("Should have failed because: `OVER` is uppercase.");
+		}
+	}
+}
