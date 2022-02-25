869d611980cfd7f0782d2114104cafa5409c4bf9
Pressacco <5507864+pressacco@users.noreply.github.com>
2022-02-24 19:09:01 -0500

    Refactor : Renamed `DetectTimeGap` to `MeasureElapsedTime` to clarify intent of the analysis tool. #159

--- Src/BlueDotBrigade.Weevil.Gui/Filter/FilterResultsViewModel.Commands.cs
+++ Src/BlueDotBrigade.Weevil.Gui/Filter/FilterResultsViewModel.Commands.cs
@@ -187,13 +187,13 @@ namespace BlueDotBrigade.Weevil.Gui.Filter
 		public ICommand SaveCommentSummaryCommand => new UiBoundCommand(SaveCommentSummary, () => this.IsMenuEnabled);
 
 		[SafeForDependencyAnalysis]
-		public ICommand DetectTimeGapUiOnlyCommand => new UiBoundCommand(
-			() => Analyze(AnalysisType.TimeGapUiOnly), 
+		public ICommand MeasureElapsedTimeUiThreadCommand => new UiBoundCommand(
+			() => Analyze(AnalysisType.ElapsedTimeUiThread), 
 			() => this.IsMenuEnabled);
 
 		[SafeForDependencyAnalysis]
-		public ICommand DetectTimeGapCommand => new UiBoundCommand(
-			() => Analyze(AnalysisType.TimeGap),
+		public ICommand MeasureElapsedTimeCommand => new UiBoundCommand(
+			() => Analyze(AnalysisType.ElapsedTime),
 			() => this.IsMenuEnabled);
 
 		[SafeForDependencyAnalysis]
