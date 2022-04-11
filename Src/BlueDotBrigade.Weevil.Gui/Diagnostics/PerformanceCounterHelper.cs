36a579e0eae8ec28be5ea48e47f5bf487fbb9a7f
Pressacco <5507864+pressacco@users.noreply.github.com>
2022-04-11 19:48:20 -0400

    New: Created `TaskManager` class that can be used to determine how much "Memory" (Private Working Set) is being used by an application. #277

---
+++ Src/BlueDotBrigade.Weevil.Gui/Diagnostics/PerformanceCounterHelper.cs
@@ -0,0 +1,44 @@
+﻿namespace BlueDotBrigade.Weevil.Gui.Diagnostics
+{
+	using System.Diagnostics;
+	using System.Linq;
+
+	internal static class PerformanceCounterHelper
+	{
+		/// <summary>
+		/// Retrieves the <see cref="PerformanceCounter"/> instance name for the given <paramref name="process"/>.
+		/// </summary>
+		/// <param name="process">The desired process.</param>
+		/// <returns>
+		/// Returns an instance name.
+		/// </returns>
+		/// <remarks>
+		/// .NET Framework 4.8 requires that you use an instance name, instead of a process identifier (PID),
+		/// to retrieve <see cref="PerformanceCounter"/> values.
+		/// </remarks>
+		/// <seealso cref="PerformanceCounter"/>
+		public static string GetInstanceName(Process process)
+		{
+			var result = string.Empty;
+
+			var category = new PerformanceCounterCategory("Process");
+			var instanceNames = category
+				.GetInstanceNames()
+				.Where(x => x.Contains(process.ProcessName));
+
+			foreach (var instanceName in instanceNames)
+			{
+				using (var performanceCounter = new PerformanceCounter("Process", "ID Process", instanceName, true))
+				{
+					if (performanceCounter.RawValue == process.Id)
+					{
+						result = instanceName;
+						break;
+					}
+				}
+			}
+
+			return result;
+		}
+	}
+}
---
+++ Src/BlueDotBrigade.Weevil.Gui/Diagnostics/TaskManager.cs
@@ -0,0 +1,27 @@
+﻿namespace BlueDotBrigade.Weevil.Gui.Diagnostics
+{
+	using System.Diagnostics;
+	using BlueDotBrigade.Weevil.IO;
+
+	internal static class TaskManager
+	{
+		/// <summary>
+		/// Determines the amount of physical memory (RAM) that is being used by the given <paramref name="process"/>.
+		/// </summary>
+		/// <param name="process">Represents the process that will be analyzed.</param>
+		/// <returns>
+		/// </returns>
+		public static StorageUnit GetPrivateWorkingSet(Process process)
+		{
+			StorageUnit privateWorkingSet;
+
+			var nameToUseForMemory = PerformanceCounterHelper.GetInstanceName(process);
+			using (var procPerfCounter = new PerformanceCounter("Process", "Working Set - Private", nameToUseForMemory))
+			{
+				// This RawValue is measured in bytes.
+				privateWorkingSet = new StorageUnit(procPerfCounter.RawValue); 
+			}
+			return privateWorkingSet;
+		}
+	}
+}
