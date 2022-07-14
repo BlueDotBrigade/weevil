e95081c57197524ccbbc5e52f8a782410827c70c
Pressacco <5507864+pressacco@users.noreply.github.com>
2022-07-13 20:41:15 -0400

    Test : Added unit tests for `TimeSpanExtensions` #307

---
+++ Tst/BlueDotBrigade.Weevil.Common-UnitTests/TimeSpanExtensionTests.cs
@@ -0,0 +1,73 @@
+﻿namespace BlueDotBrigade.Weevil.Common
+{
+	using System;
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	[TestClass]
+	public class TimeSpanExtensionTests
+	{
+		[TestMethod]
+		public void ToHumanReadable_TimeSpanMinimum_ReturnsDefault()
+		{
+			var period = TimeSpan.MinValue;
+
+			Assert.AreEqual("--.---s", period.ToHumanReadable());
+		}
+
+		[TestMethod]
+		public void ToHumanReadable_TimeSpanMaximum_ReturnsDefault()
+		{
+			var period = TimeSpan.MaxValue;
+
+			Assert.AreEqual("--.---s", period.ToHumanReadable());
+		}
+
+		[TestMethod]
+		public void ToHumanReadable_LessThan1Minute_ReturnsSecondsFormat()
+		{
+			var period = TimeSpan.FromMilliseconds(1);
+
+			Assert.AreEqual("00.001s", period.ToHumanReadable());
+		}
+
+		[TestMethod]
+		public void ToHumanReadable_MoreThan1Minute_ReturnsHourFormat()
+		{
+			var period = TimeSpan.FromMinutes(2);
+
+			Assert.AreEqual("00:02:00", period.ToHumanReadable());
+		}
+
+		[TestMethod]
+		public void ToHumanReadable_MoreThan1Day_ReturnsDayFormat()
+		{
+			var period = TimeSpan.FromDays(3);
+
+			Assert.AreEqual("3.00:00:00", period.ToHumanReadable());
+		}
+
+		[TestMethod]
+		public void ToHumanReadable_LessThan1MinuteNegative_ReturnsSecondsFormat()
+		{
+			var period = TimeSpan.FromMilliseconds(-1);
+
+			Assert.AreEqual("-00.001s", period.ToHumanReadable());
+		}
+
+		[TestMethod]
+		public void ToHumanReadable_MoreThan1MinuteNegative_ReturnsHourFormat()
+		{
+			var period = TimeSpan.FromMinutes(-2);
+
+			Assert.AreEqual("-00:02:00", period.ToHumanReadable());
+		}
+
+		[TestMethod]
+		public void ToHumanReadable_MoreThan1DayNegative_ReturnsDayFormat()
+		{
+			var period = TimeSpan.FromDays(-3);
+
+			Assert.AreEqual("-3.00:00:00", period.ToHumanReadable());
+		}
+	}
+}
