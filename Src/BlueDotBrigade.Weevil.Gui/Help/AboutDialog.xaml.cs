400b65ef730d1b67617cb5c39e6c7ab6dda36c30
Pressacco <5507864+pressacco@users.noreply.github.com>
2022-07-22 07:11:46 -0400

    Fix : Displaying third-party notices using the Windows default assigned application. #290

--- Src/BlueDotBrigade.Weevil.Gui/Help/AboutDialog.xaml.cs
+++ Src/BlueDotBrigade.Weevil.Gui/Help/AboutDialog.xaml.cs
@@ -162,6 +162,6 @@
 			return fileSize;
 		}
 
-		public ICommand ShowThirdPartyNoticesCommand => new UiBoundCommand(() => WindowsProcess.Start(WindowsProcessType.FileExplorer, _thirdPartyNoticesPath));
+		public ICommand ShowThirdPartyNoticesCommand => new UiBoundCommand(() => WindowsProcess.Start(WindowsProcessType.DefaultApplication, _thirdPartyNoticesPath));
 	}
 }
