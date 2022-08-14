e1bee22ce46d66bb139df10e70ce4bc99feac6cf
Pressacco <5507864+pressacco@users.noreply.github.com>
2022-08-14 08:56:39 -0400

    Test : Created a `Open empty file` SpecFlow scenario. #325

---
+++ Tst/BlueDotBrigade.Weevil.Core-FunctionalTests/ImplicitUsings.cs
@@ -0,0 +1,8 @@
+﻿global using FluentAssertions;
+
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+global using TechTalk.SpecFlow;
+global using TechTalk.SpecFlow.Assist;
+
+global using BlueDotBrigade.DatenLokator.TestsTools.UnitTesting;
