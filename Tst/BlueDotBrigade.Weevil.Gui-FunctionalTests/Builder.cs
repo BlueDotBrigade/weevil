02b13d0bb0661f91bea1c0e2626f5c9b292abc1b
Pressacco <5507864+pressacco@users.noreply.github.com>
2023-09-30 07:15:27 -0400

    New : Removed the reference to `Moq`... note that this change WILL crash Weevil. #391

--- Tst/BlueDotBrigade.Weevil.Gui-FunctionalTests/Builder.cs
+++ Tst/BlueDotBrigade.Weevil.Gui-FunctionalTests/Builder.cs
@@ -9,46 +9,49 @@
 	using BlueDotBrigade.DatenLokator.TestsTools;
 	using BlueDotBrigade.Weevil.Gui.Filter;
 	using BlueDotBrigade.Weevil.Gui.Threading;
-	using Moq;
 
 	internal class Builder
 	{
 		public MainWindowViewModel GetMainWindow()
 		{
-			var window = new Mock<Window>();
+			//var window = new Mock<Window>();
 
-			var uiDispatcher = new Mock<IUiDispatcher>();
-			uiDispatcher
-				.Setup(x => x.Invoke(It.IsAny<Action>()))
-				.Callback((Action a) => a.Invoke());
+			//var uiDispatcher = new Mock<IUiDispatcher>();
+			//uiDispatcher
+			//	.Setup(x => x.Invoke(It.IsAny<Action>()))
+			//	.Callback((Action a) => a.Invoke());
 
-			var bulletinMediator = new Mock<IBulletinMediator>();
+			//var bulletinMediator = new Mock<IBulletinMediator>();
 
-			var viewModel = new MainWindowViewModel(
-				uiDispatcher.Object, 
-				window.Object, 
-				bulletinMediator.Object);
+			//var viewModel = new MainWindowViewModel(
+			//	uiDispatcher.Object, 
+			//	window.Object, 
+			//	bulletinMediator.Object);
 
-			return viewModel;
+			//return viewModel;
+
+			return null;
 		}
 
 		public FilterResultsViewModel Get()
 		{
-			var window = new Mock<Window>();
+			//var window = new Mock<Window>();
 
-			var uiDispatcher = new Mock<IUiDispatcher>();
-			uiDispatcher
-				.Setup(x => x.Invoke(It.IsAny<Action>()))
-				.Callback((Action a) => a.Invoke());
+			//var uiDispatcher = new Mock<IUiDispatcher>();
+			//uiDispatcher
+			//	.Setup(x => x.Invoke(It.IsAny<Action>()))
+			//	.Callback((Action a) => a.Invoke());
 
-			var bulletinMediator = new Mock<IBulletinMediator>();
+			//var bulletinMediator = new Mock<IBulletinMediator>();
 
-			var viewModel = new FilterResultsViewModel(
-				window.Object,
-				uiDispatcher.Object,
-				bulletinMediator.Object);
+			//var viewModel = new FilterResultsViewModel(
+			//	window.Object,
+			//	uiDispatcher.Object,
+			//	bulletinMediator.Object);
 
-			return viewModel;
+			//return viewModel;
+
+			return null;
 		}
 	}
 }
