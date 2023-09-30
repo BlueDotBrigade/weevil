72f823b843912f1ca6d55ab5adddb18c71a78e46
Pressacco <5507864+Pressacco@users.noreply.github.com>
2023-09-30 19:02:38 +0000

    New : Replaced the old Moq code with NSubstitute. #391

--- Tst/BlueDotBrigade.Weevil.Core-UnitTests/Analysis/SeverityMetricsTest.cs
+++ Tst/BlueDotBrigade.Weevil.Core-UnitTests/Analysis/SeverityMetricsTest.cs
@@ -2,27 +2,28 @@
 {
 	using Data;
 	using Microsoft.VisualStudio.TestTools.UnitTesting;
+	using NSubstitute;
 
 	[TestClass]
 	public class SeverityMetricsTest
 	{
-		//[TestMethod]
-		//public void AdditionOperator_()
-		//{
-		//	var record = Substitute.For<IRecord>();
-		//	record.Setup(x => x.Severity).Returns(SeverityType.Information);
+		[TestMethod]
+		public void AdditionOperator_()
+		{
+			var record = Substitute.For<IRecord>();
+			record.Severity.Returns(SeverityType.Information);
 
-		//	var metricsA = new SeverityMetrics();
-		//	metricsA.Count(record.Object); // Information=1
-		//	metricsA.Count(record.Object); // Information=2
-		//	metricsA.Count(record.Object); // Information=3
+			var metricsA = new SeverityMetrics();
+			metricsA.Count(record); // Information=1
+			metricsA.Count(record); // Information=2
+			metricsA.Count(record); // Information=3
 
-		//	var metricsB = new SeverityMetrics();
-		//	metricsB.Count(record.Object); // Information=1
+			var metricsB = new SeverityMetrics();
+			metricsB.Count(record); // Information=1
 
-		//	SeverityMetrics result = metricsA + metricsB;
+			SeverityMetrics result = metricsA + metricsB;
 
-		//	Assert.AreEqual(4, result.Information);
-		//}
+			Assert.AreEqual(4, result.Information);
+		}
 	}
 }
--- Tst/BlueDotBrigade.Weevil.Core-UnitTests/Filter/Expressions/PlainText/PlainTextExpressionTest.cs
+++ Tst/BlueDotBrigade.Weevil.Core-UnitTests/Filter/Expressions/PlainText/PlainTextExpressionTest.cs
@@ -2,41 +2,42 @@
 {
 	using Data;
 	using Microsoft.VisualStudio.TestTools.UnitTesting;
+	using NSubstitute;
 
 	[TestClass]
 	public class PlainTextExpressionTest
 	{
-		//[TestMethod]
-		//public void IsMatch_IsCaseSensitive_ReturnsTrue()
-		//{
-		//	var record = Substitute.For<IRecord>();
-		//	record.Setup(x => x.Content).Returns("The quick brown fox jumps over the lazy dog");
+		[TestMethod]
+		public void IsMatch_IsCaseSensitive_ReturnsTrue()
+		{
+			var record = Substitute.For<IRecord>();
+			record.Content.Returns("The quick brown fox jumps over the lazy dog");
 
-		//	var expression = new PlainTextExpression("FOX", false);
+			var expression = new PlainTextExpression("FOX", false);
 
-		//	Assert.IsTrue(expression.IsMatch(record.Object));
-		//}
+			Assert.IsTrue(expression.IsMatch(record));
+		}
 
-		//[TestMethod]
-		//public void IsMatch_IsNotCaseSensitive_ReturnsFalse()
-		//{
-		//	var record = Substitute.For<IRecord>();
-		//	record.Setup(x => x.Content).Returns("The quick brown fox jumps over the lazy dog");
+		[TestMethod]
+		public void IsMatch_IsNotCaseSensitive_ReturnsFalse()
+		{
+			var record = Substitute.For<IRecord>();
+			record.Content.Returns("The quick brown fox jumps over the lazy dog");
 
-		//	var expression = new PlainTextExpression("FOX", true);
+			var expression = new PlainTextExpression("FOX", true);
 
-		//	Assert.IsFalse(expression.IsMatch(record.Object));
-		//}
+			Assert.IsFalse(expression.IsMatch(record));
+		}
 
-		//[TestMethod]
-		//public void IsMatch_ExpressionNotInValue_ReturnsFalse()
-		//{
-		//	var record = Substitute.For<IRecord>();
-		//	record.Setup(x => x.Content).Returns("The quick brown fox jumps over the lazy dog");
+		[TestMethod]
+		public void IsMatch_ExpressionNotInValue_ReturnsFalse()
+		{
+			var record = Substitute.For<IRecord>();
+			record.Content.Returns("The quick brown fox jumps over the lazy dog");
 
-		//	var expression = new PlainTextExpression("dinosaur", true);
+			var expression = new PlainTextExpression("dinosaur", true);
 
-		//	Assert.IsFalse(expression.IsMatch(record.Object));
-		//}
+			Assert.IsFalse(expression.IsMatch(record));
+		}
 	}
 }
