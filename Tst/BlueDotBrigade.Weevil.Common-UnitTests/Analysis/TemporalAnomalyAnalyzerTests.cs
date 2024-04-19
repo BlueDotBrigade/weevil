9578553eb90714438c38a04ba159111f1a676dc2
Pressacco <5507864+pressacco@users.noreply.github.com>
2024-04-19 19:17:07 -0400

    Fix : Addressed unit test compiler errors. #191

--- Tst/BlueDotBrigade.Weevil.Common-UnitTests/Analysis/TemporalAnomalyAnalyzerTests.cs
+++ Tst/BlueDotBrigade.Weevil.Common-UnitTests/Analysis/TemporalAnomalyAnalyzerTests.cs
@@ -1,15 +1,10 @@
 ﻿namespace BlueDotBrigade.Weevil.Common.Analysis
 {
 	using System.Collections.Immutable;
-	using BlueDotBrigade.Weevil.Analysis;
-	using BlueDotBrigade.Weevil.Data;
-	using BlueDotBrigade.Weevil.IO;
-	using BlueDotBrigade.Weevil.TestingTools.Data;
-	using System.Collections.Immutable;
-	using BlueDotBrigade.Weevil.Analysis;
-	using BlueDotBrigade.Weevil.Data;
-	using BlueDotBrigade.Weevil.IO;
-	using BlueDotBrigade.Weevil.TestingTools.Data;
+	using global::BlueDotBrigade.Weevil.Analysis;
+	using global::BlueDotBrigade.Weevil.Data;
+	using global::BlueDotBrigade.Weevil.IO;
+	using global::BlueDotBrigade.Weevil.TestingTools.Data;
 	using Microsoft.VisualStudio.TestTools.UnitTesting;
 	using NSubstitute;
 
@@ -21,7 +16,7 @@
 			[TestMethod]
 			public void Analyze_NotInChronologicalOrder_CommentUpdated()
 			{
-				ImmutableArray<IRecord> records = RecordFactory.Create()
+				ImmutableArray<IRecord> records = R.Create()
 					.WithCreatedAt(0, "10:00:00")
 					.WithCreatedAt(1, "10:15:00")
 					.WithCreatedAt(2, "10:45:00")
@@ -39,35 +34,6 @@
 
 				Assert.AreEqual("TemporalAnomaly: -00:15:00", records[3].Metadata.Comment);
 			}
-		}
-	}
-		}
-	}
-	// END: be15d9bcejpp
-
-	[TestClass]
-	public class TemporalAnomalyAnalyzerTests
-	{
-		[TestMethod]
-		public void Analyze_NotInChronologicalOrder_CommentUpdated()
-		{
-			ImmutableArray<IRecord> records = R.Create()
-				.WithCreatedAt(0, "10:00:00")
-				.WithCreatedAt(1, "10:15:00")
-				.WithCreatedAt(2, "10:45:00")
-				.WithCreatedAt(3, "10:30:00") // out of order
-				.WithCreatedAt(4, "11:00:00")
-				.GetRecords();
-
-			var dialog = Substitute.For<IUserDialog>();
-			dialog
-				.ShowUserPrompt(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<string>())
-    			.Returns("0");
-
-			var analyzer = new TemporalAnomalyAnalyzer();
-			analyzer.Analyze(records, string.Empty, dialog, canUpdateMetadata: true);
-
-			Assert.AreEqual("TemporalAnomaly: -00:15:00", records.RecordAtLineNumber(3).Metadata.Comment);
 		}
 	}
 }
