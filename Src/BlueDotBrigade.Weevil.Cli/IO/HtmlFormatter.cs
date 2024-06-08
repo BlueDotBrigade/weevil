fab3837bdb43c626003e16894897b7b0510ec192
Pressacco <5507864+pressacco@users.noreply.github.com>
2024-06-08 06:38:55 -0400

    New : Can now customize how output is written to the console. #156

---
+++ Src/BlueDotBrigade.Weevil.Cli/IO/HtmlFormatter.cs
@@ -0,0 +1,17 @@
+﻿namespace BlueDotBrigade.Weevil.Cli.IO
+{
+	using System.Diagnostics;
+	using System;
+
+	internal sealed class HtmlFormatter : IOutputFormatter
+	{
+		private int _numberedItemCounter = 1;
+
+		public string AsText(string message) => message;
+		public string AsHeading(string message) => $"<h1>{message}</h1>";
+		public string AsBullet(string message) => $"<li>{message}</li>";
+		public string AsNumbered(string message) => $"<li>{_numberedItemCounter++}. {message}</li>";
+		public string AsError(string message) => $"<span style='color:red;'>ERROR: {message}</span>";
+		public void ResetNumbering() => _numberedItemCounter = 1;
+	}
+}
