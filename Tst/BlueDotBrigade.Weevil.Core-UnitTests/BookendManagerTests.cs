135b8bd8d45327bdd9a4cf93ea03cd4135c484b8
Pressacco <5507864+pressacco@users.noreply.github.com>
2024-11-20 21:20:00 -0500

    Fix : Updated the `BookendManagerTests` unit tests. #375

--- Tst/BlueDotBrigade.Weevil.Core-UnitTests/BookendManagerTests.cs
+++ Tst/BlueDotBrigade.Weevil.Core-UnitTests/BookendManagerTests.cs
@@ -4,107 +4,106 @@ using BlueDotBrigade.Weevil.Core;
 
 namespace BlueDotBrigade.Weevil.Core.UnitTests
 {
-    [TestClass]
-    public class RegionManagerTests
-    {
-        private BookendManager _bookendManager;
+	[TestClass]
+	public class BookendManagerTests
+	{
+		private BookendManager _bookendManager;
 
-        [TestInitialize]
-        public void Setup()
-        {
-            _bookendManager = new BookendManager();
-        }
+		[TestInitialize]
+		public void Setup()
+		{
+			_bookendManager = new BookendManager();
+		}
 
-        [TestMethod]
-        public void MarkEnd_AfterStart_BookendCreated()
-        {
-            // Arrange
-            int startIndex = 800;
-            int endIndex = 900;
-            _bookendManager.MarkStart(startIndex);
+		[TestMethod]
+		public void MarkEnd_AfterStart_BookendCreated()
+		{
+			// Arrange
+			int startIndex = 800;
+			int endIndex = 900;
+			_bookendManager.MarkStart(startIndex);
 
-            // Act
-            _bookendManager.MarkEnd(endIndex);
+			// Act
+			_bookendManager.MarkEnd(endIndex);
 
-            // Assert
-            Assert.AreEqual(1, _bookendManager.Bookends.Length);
-            Assert.AreEqual(startIndex, _bookendManager.Bookends[0].StartLineNumber);
-            Assert.AreEqual(endIndex, _bookendManager.Bookends[0].EndLineNumber);
-        }
+			// Assert
+			_bookendManager.Bookends.Length.Should().Be(1);
+			_bookendManager.Bookends[0].StartLineNumber.Should().Be(startIndex);
+			_bookendManager.Bookends[0].EndLineNumber.Should().Be(endIndex);
+		}
 
-        [TestMethod]
-        [ExpectedException(typeof(InvalidOperationException))]
-        public void MarkEnd_WithoutStart_ThrowsInvalidOperationException()
-        {
-            // Arrange
-            int endIndex = 900;
+		[TestMethod]
+		public void MarkEnd_WithoutStart_ThrowsInvalidOperationException()
+		{
+			// Arrange
+			int endIndex = 900;
 
-            // Act
-            _bookendManager.MarkEnd(endIndex);
+			// Act
+			Action act = () => _bookendManager.MarkEnd(endIndex);
 
-            // Assert handled by ExpectedException
-        }
+			// Assert
+			act.Should().Throw<InvalidOperationException>();
+		}
 
-        [TestMethod]
-        public void Bookends_CreateMultipleBookends_ReturnsAllBookends()
-        {
-            // Arrange
-            _bookendManager.MarkStart(800);
-            _bookendManager.MarkEnd(900);
+		[TestMethod]
+		public void Bookends_CreateMultipleBookends_ReturnsAllBookends()
+		{
+			// Arrange
+			_bookendManager.MarkStart(800);
+			_bookendManager.MarkEnd(900);
 
-            _bookendManager.MarkStart(950);
-            _bookendManager.MarkEnd(1000);
+			_bookendManager.MarkStart(950);
+			_bookendManager.MarkEnd(1000);
 
-            // Act
-            // Assert
-            Assert.AreEqual(2, _bookendManager.Bookends.Length);
-            Assert.AreEqual(800, _bookendManager.Bookends[0].StartLineNumber);
-            Assert.AreEqual(900, _bookendManager.Bookends[0].EndLineNumber);
-            Assert.AreEqual(950, _bookendManager.Bookends[1].StartLineNumber);
-            Assert.AreEqual(1000, _bookendManager.Bookends[1].EndLineNumber);
-        }
+			// Act & Assert
+			_bookendManager.Bookends.Length.Should().Be(2);
+			_bookendManager.Bookends[0].StartLineNumber.Should().Be(800);
+			_bookendManager.Bookends[0].EndLineNumber.Should().Be(900);
+			_bookendManager.Bookends[1].StartLineNumber.Should().Be(950);
+			_bookendManager.Bookends[1].EndLineNumber.Should().Be(1000);
+		}
 
-        [TestMethod]
-        public void Clear_ExistingBookends_AllBookendsDeleted()
-        {
-            // Arrange
-            _bookendManager.MarkStart(800);
-            _bookendManager.MarkEnd(900);
+		[TestMethod]
+		public void Clear_ExistingBookends_AllBookendsDeleted()
+		{
+			// Arrange
+			_bookendManager.MarkStart(800);
+			_bookendManager.MarkEnd(900);
 
-            // Act
-            _bookendManager.Clear();
+			// Act
+			_bookendManager.Clear();
 
-            // Assert
-            Assert.AreEqual(0, _bookendManager.Bookends.Length);
-        }
+			// Assert
+			_bookendManager.Bookends.Length.Should().Be(0);
+		}
 
-        [TestMethod]
-        public void Clear_IndexIsWithinBookend_BookendDeleted()
-        {
-            // Arrange
-            _bookendManager.MarkStart(800);
-            _bookendManager.MarkEnd(900);
+		[TestMethod]
+		public void Clear_IndexIsWithinBookend_BookendDeleted()
+		{
+			// Arrange
+			_bookendManager.MarkStart(800);
+			_bookendManager.MarkEnd(900);
 
-            // Act
-            _bookendManager.Clear(850);
+			// Act
+			_bookendManager.Clear(850);
 
-            // Assert
-            Assert.AreEqual(0, _bookendManager.Bookends.Length);
-        }
+			// Assert
+			_bookendManager.Bookends.Length.Should().Be(0);
+		}
 
-        [TestMethod]
-        [ExpectedException(typeof(InvalidOperationException))]
-        public void Clear_LineNumberIsOutsideOfBookend_Throws()
-        {
-            // Arrange
-            _bookendManager.MarkStart(800);
-            _bookendManager.MarkEnd(900);
+		[TestMethod]
+		public void Clear_LineNumberIsOutsideOfBookend_ReturnsFalseAndKeepsBookend()
+		{
+			// Arrange
+			_bookendManager.MarkStart(800);
+			_bookendManager.MarkEnd(900);
 
-            // Act
-            _bookendManager.Clear(100);
+			// Act
+			var wasCleared = _bookendManager.Clear(100);
 
-            // Assert
-            Assert.AreEqual(1, _bookendManager.Bookends.Length);
-        }
-    }
+			// Assert
+			wasCleared.Should().BeFalse();
+			_bookendManager.Bookends.Length.Should().Be(1);
+		}
+	}
 }
