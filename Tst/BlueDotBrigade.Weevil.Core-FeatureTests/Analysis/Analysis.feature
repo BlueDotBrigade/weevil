16a1e31a9a17a6631b6962d3368dd9cab8bc121f
Pressacco <5507864+pressacco@users.noreply.github.com>
2025-01-11 15:26:51 -0500

    New : Added Gherkin scenario `Detect records in wrong chronological order` #191

--- Tst/BlueDotBrigade.Weevil.Core-FeatureTests/Analysis/Analysis.feature
+++ Tst/BlueDotBrigade.Weevil.Core-FeatureTests/Analysis/Analysis.feature
@@ -2,6 +2,11 @@
 
 A short summary of the feature
 
+Scenario: Detect records in wrong chronological order
+	Given that the default log file is open
+	When using temporal anomaly analysis with a threshold of 1 ms
+	Then the flagged record count will be 1
+
 Scenario: Detect time gaps greater than threshold
     Given that the default log file is open
     When using elapsed time analysis with a threshold of 14 sec
--- Tst/BlueDotBrigade.Weevil.Core-FeatureTests/Analysis/AnalysisSteps.cs
+++ Tst/BlueDotBrigade.Weevil.Core-FeatureTests/Analysis/AnalysisSteps.cs
@@ -11,6 +11,23 @@ namespace BlueDotBrigade.Weevil.Analysis
 			// nothing to do
 		}
 
+		[When($"using temporal anomaly analysis with a threshold of {X.TimePeriod}")]
+		public void WhenUsingTemporalAnomalyAnalysisWithAThresholdOfSec(TimeSpan threshold)
+		{
+			var thresholdString = ((int)threshold.TotalMilliseconds).ToString();
+
+			// Only a plugin knows what to ask the user.  Furthermore, the unit test has no idea about the implementation details
+			// ... E.g. How many parameters are needed? What types of parameters is the plugin expecting?
+			// TODO: re-write the `IUserDialog` interface so that the unit test doesn't care about the implementation details
+			var parameterProvider = Substitute.For<IUserDialog>();
+			parameterProvider
+				.ShowUserPrompt(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<string>())
+				.Returns(thresholdString);
+
+			this.Context.Engine.Analyzer.Analyze(AnalysisType.TemporalAnomaly, parameterProvider);
+		}
+
+
 		[When($"using elapsed time analysis with a threshold of {X.TimePeriod}")]
 		public void WhenUsingElapsedTimeAnalysisWithAThresholdOf(TimeSpan threshold)
 		{
