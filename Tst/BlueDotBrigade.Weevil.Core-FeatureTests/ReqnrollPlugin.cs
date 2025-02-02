22f00f53191879b29582017ca962300749f96d0f
Pressacco <5507864+pressacco@users.noreply.github.com>
2025-02-02 16:47:45 -0500

    Refactor : Simplified `ForceRegexExpressions` implementation. #375

---
+++ Tst/BlueDotBrigade.Weevil.Core-FeatureTests/ReqnrollPlugin.cs
@@ -0,0 +1,22 @@
+﻿using BlueDotBrigade.Weevil.Configuration.Reqnroll;
+using Reqnroll.Bindings.CucumberExpressions;
+using Reqnroll.Plugins;
+using Reqnroll.UnitTestProvider;
+
+namespace BlueDotBrigade.Weevil
+{
+	internal class ReqnrollPlugin : IRuntimePlugin
+	{
+		public void Initialize(
+			RuntimePluginEvents runtimePluginEvents, 
+			RuntimePluginParameters runtimePluginParameters,
+			UnitTestProviderConfiguration unitTestProviderConfiguration)
+		{
+			runtimePluginEvents.CustomizeGlobalDependencies += (_, args) =>
+			{
+				// register our class as ICucumberExpressionDetector
+				args.ObjectContainer.RegisterTypeAs<ForceRegexExpressions, ICucumberExpressionDetector>();
+			};
+		}
+	}
+}
--- Tst/BlueDotBrigade.Weevil.Gui-FeatureTests/Configuration/Reqnroll/ForceRegexExpressions.cs
+++ Tst/BlueDotBrigade.Weevil.Gui-FeatureTests/Configuration/Reqnroll/ForceRegexExpressions.cs
@@ -15,7 +15,7 @@ namespace BlueDotBrigade.Weevil.Gui.Configuration.Reqnroll;
 
 /// <seealso href="https://github.com/reqnroll/Reqnroll/blob/b8988c7311eb677852f83b4a1d0679a7f0e39371/docs/guides/how-to-configure-cucumber-expression-behavior.md">Reqnroll: Force using RegEx</seealso>
 /// <seealso href="https://github.com/reqnroll/Reqnroll/commit/812ffc8c06c647dfbd869507a34e60ac41927eeb">Reqnroll: Added option to override regex group matching behavior</seealso>
-public class ForceRegexDetector : ICucumberExpressionDetector
+public class ForceRegexExpressions : ICucumberExpressionDetector
 {
 	public bool IsCucumberExpression(string expression)
 	{
