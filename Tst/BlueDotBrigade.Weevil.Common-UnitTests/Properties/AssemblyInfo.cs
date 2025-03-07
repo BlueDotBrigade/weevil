433349bb7805a2cefeb40f68b4e4a609675be840
Pressacco <5507864+pressacco@users.noreply.github.com>
2025-03-06 21:23:12 -0500

    New : Updated Weevil version to `2.12.0` #197

--- Tst/BlueDotBrigade.Weevil.Common-UnitTests/Properties/AssemblyInfo.cs
+++ Tst/BlueDotBrigade.Weevil.Common-UnitTests/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("3e870ae8-8e00-4f9c-9c0a-b5e88ffb14b9")]
 
-[assembly: AssemblyVersion("2.11.0")]
-[assembly: AssemblyFileVersion("2.11.0")]
+[assembly: AssemblyVersion("2.12.0")]
+[assembly: AssemblyFileVersion("2.12.0")]
