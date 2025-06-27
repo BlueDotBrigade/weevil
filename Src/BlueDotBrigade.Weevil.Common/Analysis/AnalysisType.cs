e09b1624865b56455918e3aff8ba0a201c68b1d7
Pressacco <5507864+Pressacco@users.noreply.github.com>
2025-06-27 17:32:15 -0400

    Add Detect First analysis tool

--- Src/BlueDotBrigade.Weevil.Common/Analysis/AnalysisType.cs
+++ Src/BlueDotBrigade.Weevil.Common/Analysis/AnalysisType.cs
@@ -2,8 +2,9 @@
 {
 	public enum AnalysisType
 	{
-		DetectData,
-		DetectDataTransition,
+               DetectData,
+               DetectFirst,
+               DetectDataTransition,
 		DetectFallingEdges,
 		DetectRisingEdges,
 		/// <summary>
